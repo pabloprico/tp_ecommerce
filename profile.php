2ffc6083c82f9713b0c84dcc314d928b750ba489
pabloprico <jpablo.traverso@gmail.com>
2019-07-07 13:39:35 -0300

    Add Profile.php & stylecss update. Brian por favor cargame los estilos en el css!!!!

---
+++ profile.php
@@ -0,0 +1,61 @@
+<?php/*
+	require_once 'func/controller.php';
+
+	if (!isLoged()) {
+		header('location: login.php');
+		exit;
+	}
+	$user = $_SESSION['userLoged'];
+
+  echo "<pre>";
+  var_dump($_SESSION);
+  echo "</pre> <br>";
+  echo "<pre>";
+  var_dump($_POST);
+  echo "</pre> <br>";
+  echo "<pre>";
+  var_dump($user);
+  echo "</pre> <br>";
+*/
+ ?>
+
+ <?php
+ // Definimos variable para hacer dinamico el title e incluimos el head
+   $pageTitle = "Pagina de inicio";
+   require_once 'partials/head.php';
+  ?>
+  <html lang="es" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="css/style.css">
+      <title>Home</title>
+    </head>
+    <body>
+
+      <?php
+      require_once 'partials/header-navbar.php';
+     ?>
+		<h1>Bienvenida/o <?php echo $user['nombre'] ?></h1>
+		<p>Tu email es: <a href="mailto:<?= $user['email'] ?>"> <?= $user['email'] ?> </a></p>
+    <h2 style="text-align:center; font-size: 40px; font-family:'Lora', serif; margin-top:80px">Mi Cuenta</h2>
+		<a style="display: block; text-align:center; text-decoration:none; color: black;" href="logout.php">Salir</a>
+
+    <div id="perfil" style="display:flex; justify-content: space-around; margin-top:120px">
+
+      <div class="perfil-caja" style="margin-bottom: 150px;">
+        <a href="#" style="font-family:'Lora'; font-size: 28px;  text-decoration: none; color: black">Historial de Compras</a>
+      </div>
+
+      <div class="perfil-caja">
+        <a href="#" style="font-family:'Lora'; font-size: 28px; text-decoration: none; color: black">Detalles de la Cuenta</a>
+      </div>
+
+    </div>
+
+    <?php
+      require_once 'partials/footer.php';
+     ?>
+
+    </body>
+  </html>
